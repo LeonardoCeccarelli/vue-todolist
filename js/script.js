7159b8dffb0849859ac265cbefd5b5e6eb63629b
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-02 15:49:35 +0100

    add list item dynamically

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
 "use strict"
 
 Vue.config.devtools = true;
+
+new Vue({
+    el: "#root",
+    data: {
+        listTasks: ["Fare la spesa", "Pulire il giardino"],
+        newTask: "",
+    },
+    methods: {
+        onClickBtnAdd() {
+            if (this.newTask.trim() === "") {
+                alert("Il valore inserito non contiene nulla. Riprovare")
+                return
+            }
+
+            const checkSameElement = this.listTasks.find((el) => {
+                return el.toLowerCase() === this.newTask.trim().toLowerCase()
+            })
+
+            if (checkSameElement) {
+                alert("Attenzione, il valore inserito è già presente. Aggiungerne uno nuovo.")
+                return
+            }
+
+            this.listTasks.push(this.newTask)
+            this.newTask = ""
+        }
+    }
+})
